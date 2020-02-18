d9f3e757cc4986494e95ca17eeb0391e590f229e
Alexey Solovey <solovei_94@mail.ru>
2020-02-18 17:36:43 +0400

    change repository

--- src/main/java/solovey/cft/library/repository/LoanRepository.java
+++ src/main/java/solovey/cft/library/repository/LoanRepository.java
@@ -3,6 +3,8 @@ package solovey.cft.library.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solovey.cft.library.model.Loan;
 
-public interface LoanRepository extends JpaRepository<Loan, Long> {
+import java.util.List;
 
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findLoansByClientId(Long clientId);
 }
