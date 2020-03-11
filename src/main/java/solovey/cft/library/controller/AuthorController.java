9f2c5b67b187eb856a4e4dbe004272af7fedfa76
Alexey Solovey <solovei_94@mail.ru>
2020-03-11 12:16:29 +0400

    add AuthorController

---
+++ src/main/java/solovey/cft/library/controller/AuthorController.java
@@ -0,0 +1,62 @@
+package solovey.cft.library.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import solovey.cft.library.dto.AuthorDto;
+import solovey.cft.library.dto.BookDto;
+import solovey.cft.library.service.AuthorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public AuthorDto addAuthor(@RequestBody AuthorDto authorDto) {
+        return authorService.add(authorDto);
+    }
+
+    @GetMapping
+    public List<AuthorDto> findAllAuthors() {
+        return authorService.findAllAuthors();
+    }
+
+    @GetMapping("/{name}")
+    public AuthorDto getByName(@PathVariable("name") String lastName) {
+        return authorService.findAuthorByLastName(lastName);
+    }
+
+    @GetMapping("/{id}")
+    public AuthorDto findAuthorById(@PathVariable Long id) {
+        return authorService.findAuthorById(id);
+    }
+
+    @GetMapping("/{id}/books")
+    public List<BookDto> findBooksByAuthorId(@PathVariable Long id) {
+        return authorService.findBooksByAuthorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AuthorDto updateAuthor(
+            @PathVariable Long id,
+            @Validated @RequestBody AuthorDto authorDto
+    ) {
+        authorDto.setId(id);
+        return authorService.update(authorDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthorById(id);
+    }
+
+}
