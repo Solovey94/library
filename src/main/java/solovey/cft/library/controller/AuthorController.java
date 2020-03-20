969383861b7322abafdabee09e0993890c0a8b0f
Alexey Solovey <solovei_94@mail.ru>
2020-03-20 14:45:42 +0400

    Added logging to the controllers

--- src/main/java/solovey/cft/library/controller/AuthorController.java
+++ src/main/java/solovey/cft/library/controller/AuthorController.java
@@ -2,14 +2,20 @@ package solovey.cft.library.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import solovey.cft.library.dto.AuthorDto;
 import solovey.cft.library.dto.BookDto;
+import solovey.cft.library.model.Author;
 import solovey.cft.library.service.AuthorService;
 
 import java.util.List;
 
+import static solovey.cft.library.log.message.ControllerMessages.*;
+
+
+@Slf4j
 @RestController
 @RequestMapping("authors")
 @Api(value = "Author REST Endpoint")
@@ -25,33 +31,37 @@ public class AuthorController {
     @ApiOperation(value = "Return new author")
     @PostMapping
     public AuthorDto addAuthor(@RequestBody AuthorDto authorDto) {
+        log.info(LOG_ADD_NEW, Author.class.toString(), authorDto.toString());
         return authorService.add(authorDto);
     }
 
     @ApiOperation(value = "Returns all authors")
     @GetMapping
     public List<AuthorDto> findAllAuthors() {
+        log.info(LOG_GET_ALL, Author.class.toString());
         return authorService.findAllAuthors();
     }
 
     @ApiOperation(value = "Returns author by lastName")
-    @GetMapping("/name")
-    public AuthorDto findAuthorByLastName(@RequestBody AuthorDto authorDto) {
-        String lastName = authorDto.getLastName();
+    @GetMapping("/name/{lastName}")
+    public AuthorDto findAuthorByLastName(@PathVariable String lastName) {
+        AuthorDto authorDto = authorService.findAuthorByLastName(lastName);
+        log.info(LOG_GET, Author.class.toString(), authorDto.toString());
         return authorService.findAuthorByLastName(lastName);
     }
 
     @ApiOperation(value = "Returns author by author_id")
-    @GetMapping("/id")
-    public AuthorDto findAuthorById(@RequestBody AuthorDto authorDto) {
-        Long id = authorDto.getId();
+    @GetMapping("/{id}")
+    public AuthorDto findAuthorById(@PathVariable Long id) {
+        AuthorDto authorDto = authorService.findAuthorById(id);
+        log.info(LOG_GET, AuthorDto.class.toString(), authorDto.toString());
         return authorService.findAuthorById(id);
     }
 
     @ApiOperation(value = "Returns author books")
-    @GetMapping("/books")
-    public List<BookDto> findBooksByAuthorId(@RequestBody AuthorDto authorDto) {
-        Long id = authorDto.getId();
+    @GetMapping("/{id}/books")
+    public List<BookDto> findBooksByAuthorId(@PathVariable Long id) {
+        log.info(LOG_GET_ALL, BookDto.class.toString());
         return authorService.findBooksByAuthorId(id);
     }
 
@@ -60,14 +70,15 @@ public class AuthorController {
     public AuthorDto updateAuthor(@RequestBody AuthorDto authorDto) {
         Long id = authorDto.getId();
         authorDto.setId(id);
+        log.info(LOG_UPDATE, Author.class.toString(), authorDto.toString());
         return authorService.update(authorDto);
     }
 
     @ApiOperation(value = "Delete author by author_id")
-    @DeleteMapping()
-    public void deleteAuthor(@RequestBody AuthorDto authorDto) {
-        Long id = authorDto.getId();
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id) {
         authorService.deleteAuthorById(id);
+        log.info(LOG_DELETE_BY_ID, Author.class.toString(), id);
     }
 
 }
---
+++ src/main/java/solovey/cft/library/log/message/ControllerMessages.java
@@ -0,0 +1,10 @@
+package solovey.cft.library.log.message;
+
+public class ControllerMessages {
+    public static final String LOG_ADD_NEW = "New {} with parameters: {}, was added";
+    public static final String LOG_UPDATE = "{} is updated. New parameters: {}";
+    public static final String LOG_DELETE_BY_ID = "Delete {}, with id: {}";
+    public static final String LOG_GET = "Get {}, with parameters: {}";
+    public static final String LOG_GET_ALL = "Get all {}";
+    public static final String LOG_GET_SET_BY = "Get set of {}, by: {}";
+}
